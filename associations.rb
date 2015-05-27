e37a22662d980f406e80440c7357432aa7567c25
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-27 17:25:06 -0400

    Solutions! :smile:

--- associations.rb
+++ associations.rb
@@ -13,25 +13,30 @@ end
 
 def generate_migrations
   ActiveRecord::Migration.create_table :hotels do |t|
-    #insert our columns here
+    t.string :name
+    t.integer :room_count
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :rooms do |t|
-    #insert our columns here
+    t.integer :hotel_id
+    t.integer :rate
+    t.string :location
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :bookings do |t|
-    #insert our columns here
+    t.integer :room_id
+    t.references :guest
+    t.date :check_in
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :users do |t|
-    #insert our columns here
+    t.string :name
 
     t.timestamps null: false
   end
@@ -46,25 +51,30 @@ migrate()
 
 
 class Hotel < ActiveRecord::Base
-  #insert our associations here
- 
+  has_many :rooms
+  has_many :bookings, through: :rooms
+  has_many :booked_guests, through: :bookings, source: :guest
+
   def to_s
     "#{name} with #{rooms.count} rooms"
   end
 end
 
 class Booking < ActiveRecord::Base
-  #insert our associations here
+  belongs_to :guest, class_name: "User"
+  belongs_to :room
 
 end
 
 class Room < ActiveRecord::Base
-  #insert our associations here
+  belongs_to :hotel
+  has_many :bookings
 
 end
 
 class User < ActiveRecord::Base
-  #insert our associations here
+  has_many :bookings, foreign_key: 'guest_id'
+  has_many :booked_rooms, through: :bookings, source: :room
 
 end
 
@@ -74,16 +84,16 @@ def random_loc; (('a'..'e').to_a.sample) + rand(1..5).to_s; end
 
 hotel = Hotel.create!(name: "Westin", room_count: 5)
 
-5.times do 
+5.times do
   hotel.rooms << Room.create!(
     rate: [125,200,175].sample,
     location: random_loc
-  ) 
+  )
 end
 
 user = User.create!(name: "John Smith")
 room = hotel.rooms.first
-b = Booking.create!(guest: user, room: room, check_in: Time.now)
+Booking.create!(guest: user, room: room, check_in: Time.now)
 
 line_sep("#{user.name} bookings")
 tp user.bookings
