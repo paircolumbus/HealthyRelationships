17ca22b6334a478c03a60b969e230d4a31586a62
maxdignan <maxdignan@gmail.com>
2015-12-27 19:01:01 -0500

    made the necessary changes

--- associations.rb
+++ associations.rb
@@ -13,25 +13,30 @@ end
 
 def generate_migrations
   ActiveRecord::Migration.create_table :hotels do |t|
-    #insert our columns here
+    t.string :name
+    t.integer :room_count
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :rooms do |t|
-    #insert our columns here
+    t.integer :rate
+    t.string :location
+    t.belongs_to :hotel
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :bookings do |t|
-    #insert our columns here
+    t.belongs_to :user
+    t.belongs_to :room
+    t.datetime :check_in
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :users do |t|
-    #insert our columns here
+    t.string :name
 
     t.timestamps null: false
   end
@@ -46,26 +51,29 @@ migrate()
 
 
 class Hotel < ActiveRecord::Base
-  #insert our associations here
- 
+  has_many :rooms
+  has_many :bookings, through: :rooms
+  has_many :booked_guests, through: :bookings, source: :guest
+
   def to_s
     "#{name} with #{rooms.count} rooms"
   end
 end
 
 class Booking < ActiveRecord::Base
-  #insert our associations here
+  belongs_to :room
+  belongs_to :guest, class_name: "User", foreign_key: "user_id"
 
 end
 
 class Room < ActiveRecord::Base
-  #insert our associations here
-
+  belongs_to :hotel
+  has_many :bookings
 end
 
 class User < ActiveRecord::Base
-  #insert our associations here
-
+  has_many :bookings
+  has_many :booked_rooms, through: :bookings, source: :room
 end
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE.
@@ -74,11 +82,11 @@ def random_loc; (('a'..'e').to_a.sample) + rand(1..5).to_s; end
 
 hotel = Hotel.create!(name: "Westin", room_count: 5)
 
-5.times do 
+5.times do
   hotel.rooms << Room.create!(
     rate: [125,200,175].sample,
     location: random_loc
-  ) 
+  )
 end
 
 user = User.create!(name: "John Smith")
