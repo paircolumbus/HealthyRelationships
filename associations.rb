1817be74d66ad4b37bdd7820ca14a6c3ee3dfda1
Jonah Roth <jroth@CMDVMOSXLT192.local>
2016-05-27 16:31:39 -0400

    Works, except that guest id isn't recorded in bookings

--- associations.rb
+++ associations.rb
@@ -13,28 +13,35 @@ end
 
 def generate_migrations
   ActiveRecord::Migration.create_table :hotels do |t|
-    #insert our columns here
+    t.string :name
+    t.integer :room_count
+
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :rooms do |t|
-    #insert our columns here
+    t.integer :hotel_id
+    t.integer :rate
+    t.string :location
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :bookings do |t|
-    #insert our columns here
+    t.datetime :check_in
+    t.integer :guest
+    t.integer :room
 
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :users do |t|
-    #insert our columns here
+    t.string :name
 
     t.timestamps null: false
   end
+
 end
 
 
@@ -46,25 +53,44 @@ migrate()
 
 
 class Hotel < ActiveRecord::Base
-  #insert our associations here
- 
+  has_many :rooms
+
   def to_s
     "#{name} with #{rooms.count} rooms"
+  end
+
+  def bookings
+    Booking.where(:room => rooms.map{|r| r.id})
+    # rooms.collect_concat {|r| r.bookings.to_a}
+  end
+
+  def booked_guests
+    bookings.map{|b| b.guest}.uniq
   end
 end
 
 class Booking < ActiveRecord::Base
-  #insert our associations here
+  belongs_to :room, foreign_key: :room
+  belongs_to :user, foreign_key: :guest
 
 end
 
 class Room < ActiveRecord::Base
-  #insert our associations here
+  belongs_to :hotel
+  has_many :bookings, foreign_key: :room
 
 end
 
 class User < ActiveRecord::Base
-  #insert our associations here
+  has_many :bookings, foreign_key: :guest
+
+  def bookings
+    Booking.where(:guest => id)
+  end
+
+  def booked_rooms
+    bookings.collect_concat {|b| b.room}
+  end
 
 end
 
@@ -74,16 +100,21 @@ def random_loc; (('a'..'e').to_a.sample) + rand(1..5).to_s; end
 
 hotel = Hotel.create!(name: "Westin", room_count: 5)
 
-5.times do 
+5.times do
   hotel.rooms << Room.create!(
     rate: [125,200,175].sample,
     location: random_loc
-  ) 
+  )
 end
+
+p hotel.rooms
 
 user = User.create!(name: "John Smith")
 room = hotel.rooms.first
 b = Booking.create!(guest: user, room: room, check_in: Time.now)
+
+p user
+p hotel.rooms.first.bookings
 
 line_sep("#{user.name} bookings")
 tp user.bookings
