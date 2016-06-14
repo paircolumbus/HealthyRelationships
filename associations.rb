edefa4283de5d0ae9f2a933760557f85f46bc762
Josh Meek <jmeek@covermymeds.com>
2016-06-14 17:15:27 -0400

    complete table creation

--- associations.rb
+++ associations.rb
@@ -3,85 +3,86 @@ require 'table_print'
 require 'awesome_print'
 
 def setup
-  ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+  ActiveRecord::Base.establish_connection adapter: 'sqlite3',
+                                          database: ':memory:'
 end
 
 def migrate
-  ActiveRecord::Migrator.up "db/migrate"
+  ActiveRecord::Migrator.up 'db/migrate'
 end
-
 
 def generate_migrations
   ActiveRecord::Migration.create_table :hotels do |t|
-    #insert our columns here
-
+    t.string :name
+    t.integer :room_count
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :rooms do |t|
-    #insert our columns here
-
+    t.integer  :rate
+    t.string :location
+    t.belongs_to :hotel
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :bookings do |t|
-    #insert our columns here
-
+    t.datetime :check_in
+    t.belongs_to :user
+    t.belongs_to :room
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :users do |t|
-    #insert our columns here
-
+    t.string :name
     t.timestamps null: false
   end
 end
 
-
 ## Do Not Modify These Lines ##
-setup()
-generate_migrations()
-migrate()
+setup
+generate_migrations
+migrate
 #####  ^^ Do Not Modify ^^ ####
 
-
 class Hotel < ActiveRecord::Base
-  #insert our associations here
- 
+  # insert our associations here
+
   def to_s
     "#{name} with #{rooms.count} rooms"
   end
 end
 
 class Booking < ActiveRecord::Base
-  #insert our associations here
-
+  # insert our associations here
 end
 
 class Room < ActiveRecord::Base
-  #insert our associations here
-
+  # insert our associations here
 end
 
 class User < ActiveRecord::Base
-  #insert our associations here
-
+  # insert our associations here
 end
 
-#DO NOT CHANGE ANYTHING BELOW THIS LINE.
-def line_sep(title=nil); print "\n#{title} ----\n\n"; end
-def random_loc; (('a'..'e').to_a.sample) + rand(1..5).to_s; end
+# DO NOT CHANGE ANYTHING BELOW THIS LINE.
+def line_sep(title = nil)
+  print "\n#{title} ----\n\n"
+end
 
-hotel = Hotel.create!(name: "Westin", room_count: 5)
+def random_loc
+  ('a'..'e').to_a.sample + rand(1..5).to_s
+end
 
-5.times do 
+hotel = Hotel.create!(name: 'Westin', room_count: 5)
+
+5.times do
   hotel.rooms << Room.create!(
-    rate: [125,200,175].sample,
+    rate: [125, 200, 175].sample,
     location: random_loc
-  ) 
+  )
 end
 
-user = User.create!(name: "John Smith")
+user = User.create!(name: 'John Smith')
 room = hotel.rooms.first
 b = Booking.create!(guest: user, room: room, check_in: Time.now)
 
