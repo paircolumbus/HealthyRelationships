8af9931ff248781f9eca86130f59865dde699f92
Cara Kane <ckane@cmmc02vg1aug8wl.innova.local>
2018-05-14 14:22:27 -0400

    finished challenge

--- associations.rb
+++ associations.rb
@@ -13,26 +13,27 @@ end
 
 def generate_migrations
   ActiveRecord::Migration.create_table :hotels do |t|
-    #insert our columns here
-
+    t.string :name
+    t.integer :room_count
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :rooms do |t|
-    #insert our columns here
-
+    t.integer :hotel_id
+    t.string :location
+    t.integer :rate
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :bookings do |t|
-    #insert our columns here
-
+    t.integer :room_id
+    t.integer :guest_id
+    t.datetime :check_in
     t.timestamps null: false
   end
 
   ActiveRecord::Migration.create_table :users do |t|
-    #insert our columns here
-
+    t.string :name
     t.timestamps null: false
   end
 end
@@ -46,26 +47,29 @@ migrate()
 
 
 class Hotel < ActiveRecord::Base
-  #insert our associations here
- 
+  has_many :rooms
+  has_many :bookings, through: :rooms
+  has_many :booked_guests, through: :bookings, source: :guest
+
   def to_s
     "#{name} with #{rooms.count} rooms"
   end
 end
 
 class Booking < ActiveRecord::Base
-  #insert our associations here
-
+  belongs_to :guest, :class_name => "User"
+  belongs_to :room
 end
 
 class Room < ActiveRecord::Base
-  #insert our associations here
-
+  belongs_to :hotel
+  has_many :bookings
+  has_many :guests, through: :bookings, source: :user
 end
 
 class User < ActiveRecord::Base
-  #insert our associations here
-
+  has_many :bookings, foreign_key: "guest_id"
+  has_many :booked_rooms, through: :bookings, source: :room
 end
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE.
